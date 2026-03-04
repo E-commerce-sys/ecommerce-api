e88b21ea1246de2544522b71df9571e90df9db9e
Ali Abdulkhaliq <alimajeed8110@gmail.com>
2026-03-04 12:50:55 +0300

    login and logout apis

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\Api\V1\RegisterUserController;
+use App\Http\Controllers\api\v1\UserSessionController;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -8,4 +10,8 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::post('/signup', [RegisterUserController::class, 'store']);
+Route::prefix('auth')->group(function () {
+    Route::post('/signup', [RegisterUserController::class, 'store']);
+    Route::post('/login', [UserSessionController::class, 'store']);
+    Route::post('/logout', [UserSessionController::class, 'destroy'])->middleware('auth:sanctum');
+});

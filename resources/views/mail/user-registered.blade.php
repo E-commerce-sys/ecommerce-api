113826ee1f7f2471d35b7cb9309583c096e356de
Ali Abdulkhaliq <alimajeed8110@gmail.com>
2026-03-11 12:03:58 +0300

    implement email verification feature with user registration notification

---
+++ resources/views/mail/user-registered.blade.php
@@ -0,0 +1,20 @@
+<x-mail::message>
+# Welcome to {{ config('app.name') }}! 🎉
+
+Hi {{ $user->first_name }},
+
+Thank you for signing up for {{ config('app.name') }}. We're excited to have you join our community!
+Before you start exploring and shopping on our platform, please take a moment to verify your email address. This helps us keep your account secure and ensures you can create orders. 
+Simply click the button below to confirm your email address.
+
+<x-mail::button :url="''">
+Verify My Email
+</x-mail::button>
+
+If you didn’t create an account with {{ config('app.name') }}, you can safely ignore this email.
+
+We’re happy to have you here and hope you enjoy your experience with us!
+
+Thanks,<br>
+The {{ config('app.name') }} Team
+</x-mail::message>
---
+++ resources/views/vendor/mail/html/header.blade.php
@@ -0,0 +1,8 @@
+@props(['url'])
+<tr>
+<td class="header">
+<a href="{{ $url }}" style="display: inline-block;">
+<img src="https://my-portfolio-bkt-666.s3.eu-north-1.amazonaws.com/e-commerce-logo.png" class="logo" alt="Exclusive Logo" >
+</a>
+</td>
+</tr>

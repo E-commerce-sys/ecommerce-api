e1f377a8c0f1302d516969c4deaa52664b38f335
Ali Abdulkhaliq <alimajeed8110@gmail.com>
2026-03-29 14:37:48 +0300

    add product_variant table and remove quantity from other product tables

--- database/migrations/2026_03_08_090932_create_products_table.php
+++ database/migrations/2026_03_08_090932_create_products_table.php
@@ -32,7 +32,6 @@ return new class extends Migration
 
             $table->index('effective_price');
 
-            $table->integer('stock_quantity')->default(0);
             $table->boolean('is_best_selling')->default(false);
             $table->boolean('is_featured')->default(false);
             $table->boolean('is_new_arrival')->default(false);
@@ -56,7 +55,6 @@ return new class extends Migration
             $table->string('name');
             $table->string('hex_code');
             $table->foreignId('product_id')->constrained('products')->onDelete('cascade');
-            $table->integer('stock_quantity')->default(0);
 
             // Composite keys
             $table->unique(['id', 'product_id']);
@@ -69,12 +67,20 @@ return new class extends Migration
             $table->string('name');
             $table->string('size_label');
             $table->decimal('extra_price', 8, 2)->default(0);
-            $table->integer('stock_quantity')->default(0);
             $table->foreignId('product_id')->constrained('products')->onDelete('cascade');
 
             // Composite keys
             $table->unique(['id', 'product_id']);
 
+            $table->timestamps();
+        });
+
+        Schema::create('product_variants', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('product_id')->constrained('products')->onDelete('cascade');
+            $table->foreignId('color_id')->constrained('product_colors')->onDelete('cascade');
+            $table->foreignId('size_id')->constrained('product_sizes')->onDelete('cascade');
+            $table->integer('stock')->default(0);
             $table->timestamps();
         });
     }

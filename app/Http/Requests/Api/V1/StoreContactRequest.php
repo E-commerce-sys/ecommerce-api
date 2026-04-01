c4d915ff5952178cc6d3cea21c7346d8c22e526f
Ali Abdulkhaliq <alimajeed8110@gmail.com>
2026-04-01 15:38:31 +0300

    contact validation rule

--- app/Http/Requests/Api/V1/StoreContactRequest.php
+++ app/Http/Requests/Api/V1/StoreContactRequest.php
@@ -24,7 +24,7 @@ class StoreContactRequest extends BaseContactRequest
         return [
             'data.attributes.name' => 'required|string|alpha|min:2|max:32',
             'data.attributes.email' => 'required|email',
-            'data.attributes.phone' => ['required', 'integer', 'digits:10', 'min:6', 'max:15'],
+            'data.attributes.phone' => ['required', 'digits_between:6,15'],
             'data.attributes.message' => 'required|string|max:500',
         ];
     }

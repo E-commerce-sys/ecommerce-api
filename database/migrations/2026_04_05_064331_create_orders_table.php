31bc3788b977de685b2ea7e623ed4971e82227ac
Ali Abdulkhaliq <alimajeed8110@gmail.com>
2026-04-05 10:35:38 +0300

    order items table

--- database/migrations/2026_04_05_064331_create_orders_table.php
+++ database/migrations/2026_04_05_064331_create_orders_table.php
@@ -23,6 +23,14 @@ return new class extends Migration
             $table->timestamps();
         });
 
+        Schema::create('order_items', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('order_id')->constrained()->onDelete('cascade');
+            $table->foreignId('product_variant_id')->constrained('product_variants')->onDelete('set null');
+            $table->integer('quantity');
+            $table->decimal('unit_price', 8, 2);
+            $table->timestamps();
+        });
     }
 
     /**
@@ -31,5 +39,6 @@ return new class extends Migration
     public function down(): void
     {
         Schema::dropIfExists('orders');
+        Schema::dropIfExists('order_items');
     }
 };

4a59609674830b5961f49f45b69abf7bb52d9bdc
Ali Abdulkhaliq <alimajeed8110@gmail.com>
2026-04-05 10:13:12 +0300

    orders table

---
+++ database/migrations/2026_04_05_064331_create_orders_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade')->unique();
+            $table->string('status')->default('pending');
+            $table->foreignId('shipping_address_id')->constrained('addresses')->onDelete('set null');
+            $table->decimal('shipping_cost', 8, 2)->default(0);
+            $table->integer('discount_percentage')->default(0);
+            $table->decimal('subtotal', 8, 2)->default(0);
+            $table->decimal('total_price', 8, 2)->default(0);
+            $table->timestamps();
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};

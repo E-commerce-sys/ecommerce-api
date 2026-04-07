c1387533aeec37247e342b0210ba8f2fa2c2b26f
Ali Abdulkhaliq <alimajeed8110@gmail.com>
2026-04-07 11:30:08 +0300

    update user address

--- app/Http/Controllers/Api/V1/AddressController.php
+++ app/Http/Controllers/Api/V1/AddressController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Requests\Api\V1\StoreAddressRequest;
+use App\Http\Requests\Api\V1\UpdateAddressRequest;
 use App\Http\Resources\Api\V1\AddressResource;
 use Spatie\QueryBuilder\QueryBuilder;
 
@@ -19,5 +20,11 @@ class AddressController extends ApiController
     public function store(StoreAddressRequest $request) {
         $address = auth('sanctum')->user()->addresses()->create($request->mappedAttributes());
         return $this->success(new AddressResource($address), 'Address created successfully!', 201);
+    }
+
+    public function update(UpdateAddressRequest $request, $address_id) {
+        $address = auth('sanctum')->user()->addresses()->findOrFail($address_id);
+        $address->update($request->mappedAttributes());
+        return $this->success(new AddressResource($address), 'Address updated successfully!');
     }
 }

3e53e2e6a22cc1278dda8ff352982e17fbc25257
Ali Abdulkhaliq <alimajeed8110@gmail.com>
2026-04-15 14:31:38 +0300

    paginate orders

--- app/Http/Controllers/Api/V1/OrderController.php
+++ app/Http/Controllers/Api/V1/OrderController.php
@@ -23,7 +23,7 @@ class OrderController extends ApiController
             ->allowedIncludes(Order::allowedIncludes())
             ->allowedFilters(Order::allowedFilters())
             ->orderBy('created_at', 'desc')
-            ->get()
+            ->paginate(25)
         );
     }
 

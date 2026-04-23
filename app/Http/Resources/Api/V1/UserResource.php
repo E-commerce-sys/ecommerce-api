a1882a7b7d799450697f7362411aa6ca118b86a0
Ali Abdulkhaliq <alimajeed8110@gmail.com>
2026-04-23 16:38:24 +0300

    fix bug: orders get lazy loaded even though the route is not admin.user.index

--- app/Http/Resources/Api/V1/UserResource.php
+++ app/Http/Resources/Api/V1/UserResource.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Resources\Api\V1;
 
-use App\Http\Resources\V1\ResearchResource;
 use App\States\Arrived;
 use App\States\Cancelled;
 use Illuminate\Http\Request;
@@ -10,11 +9,6 @@ use Illuminate\Http\Resources\Json\JsonResource;
 
 class UserResource extends JsonResource
 {
-    /**
-     * Transform the resource into an array.
-     *
-     * @return array<string, mixed>
-     */
     public function toArray(Request $request): array
     {
         return [
@@ -24,21 +18,33 @@ class UserResource extends JsonResource
                 'firstName' => $this->first_name,
                 'lastName' => $this->last_name,
                 'email' => $this->email,
-                $this->mergeWhen($request->user()?->hasRole('super_admin') || $request->user()?->hasRole('admin'), [
-                    'isSuperAdmin' => $this->hasRole('super_admin'),    
-                    'isAdmin' => $this->hasRole('admin'),
-                    $this->mergeWhen($request->routeIs('admin.users.index'), [
-                        'isBlocked' => $this->blocked_at ? true : false,
-                        'isVerified' => $this->email_verified_at ? true : false,
-                        'numOfOrders' => $this->orders->count(),
-                        'numCancelledOrders' => $this->orders->where('status', Cancelled::$name)->count(),
-                        'numArrivedOrders' => $this->orders->where('status', Arrived::$name)->count(),
-                    ])
-                ]),
+
+                $this->mergeWhen(
+                    $request->user()?->hasRole('super_admin') || $request->user()?->hasRole('admin'),
+                    function () use ($request) {
+                        return [
+                            'isSuperAdmin' => $this->hasRole('super_admin'),
+                            'isAdmin' => $this->hasRole('admin'),
+
+                            $this->mergeWhen(
+                                $request->routeIs('admin.users.index'),
+                                function () {
+                                    return [
+                                        'isBlocked' => (bool) $this->blocked_at,
+                                        'isVerified' => (bool) $this->email_verified_at,
+                                        'numOfOrders' => $this->orders->count(),
+                                        'numCancelledOrders' => $this->orders->where('status', Cancelled::$name)->count(),
+                                        'numArrivedOrders' => $this->orders->where('status', Arrived::$name)->count(),
+                                    ];
+                                }
+                            ),
+                        ];
+                    }
+                ),
             ],
             'included' => [
                 'mainAddress' => new AddressResource($this->mainAddress),
-            ]
+            ],
         ];
     }
 }
